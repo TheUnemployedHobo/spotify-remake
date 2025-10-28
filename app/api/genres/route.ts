9db0d613f0a504fcd445788c4bdfc4d96985327a
Pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-10-28 14:45:43 +0330

    feat(genres): implement GET endpoint to retrieve genre data from the database

---
+++ app/api/genres/route.ts
@@ -0,0 +1,12 @@
+import db from "@/lib/db"
+import { genres } from "@/lib/db/schema/genre.schema"
+
+export async function GET() {
+  try {
+    const data = await db.select().from(genres)
+
+    return Response.json(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
