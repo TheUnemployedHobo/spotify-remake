e916e783128344a518816c46c21ad21f3e3f0991
Pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-10-29 16:49:23 +0330

    refactor(proxy): comment out session retrieval and logging in proxy function

--- proxy.ts
+++ proxy.ts
@@ -3,9 +3,9 @@ import { type NextRequest, NextResponse, type ProxyConfig } from "next/server"
 import auth from "./lib/auth"
 
 export async function proxy(req: NextRequest) {
-  const session = await auth.api.getSession({ headers: req.headers })
+  // const session = await auth.api.getSession({ headers: req.headers })
 
-  console.log(session)
+  // console.log(session)
 
   return NextResponse.next()
 }
