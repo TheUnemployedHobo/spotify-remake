9088afd21f80ddd42b5f48476338f495f4b91043
Pumpkin <mohammadmehdibeheshti@yahoo.com>
2025-10-29 22:03:27 +0330

    fix(signin): update sign-in form to use username instead of email

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -14,13 +14,12 @@ import authClient from "@/lib/auth-client"
 
 export default function Page() {
   const handleAction = async (f: FormData) => {
-    const email = f.get("email") as string
+    const username = f.get("username") as string
     const password = f.get("password") as string
     const remember = f.get("remember") as string
 
-    await authClient.signIn.email({
+    await authClient.signIn.username({
       callbackURL: "/",
-      email,
       fetchOptions: {
         onError() {
           toast.error("Failed to sign in. Please check your credentials and try again")
@@ -31,6 +30,7 @@ export default function Page() {
       },
       password,
       rememberMe: Boolean(remember),
+      username,
     })
   }
 
@@ -39,12 +39,12 @@ export default function Page() {
       <Card className="w-full rounded-xs sm:w-96">
         <CardHeader>
           <CardTitle>Sign into your account</CardTitle>
-          <CardDescription>Use your credentials to access your account.</CardDescription>
+          <CardDescription>Use your credentials to access your account</CardDescription>
         </CardHeader>
         <CardContent className="space-y-5">
           <Label className="block space-y-2">
-            <span className="block">Email</span>
-            <Input name="email" placeholder="admin@example.com" required type="email" />
+            <span className="block">Username</span>
+            <Input name="username" placeholder="example" required type="text" />
           </Label>
           <Label className="block space-y-2">
             <span className="block">Password</span>
@@ -52,7 +52,7 @@ export default function Page() {
           </Label>
           <Label>
             <Checkbox defaultChecked name="remember" />
-            <span>Remember me</span>
+            <span>Remember me for 30 days</span>
           </Label>
           <SubmitButton text="Sign up" />
         </CardContent>
